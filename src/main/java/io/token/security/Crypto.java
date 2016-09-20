9e9099d0f8f226ef85b13f022e99686707cbc3cc
Alex Budt <alex@token.io>
2016-09-19 18:16:26 -0700

    javadoc added

--- src/main/java/io/token/security/Crypto.java
+++ src/main/java/io/token/security/Crypto.java
@@ -42,7 +42,7 @@ public final class Crypto {
     }
 
     /**
-     * Signes the specified payload using given key
+     * Signs the specified payload using given key
      *
      * @param key the key to be used for signing
      * @param payload the payload to be signed
