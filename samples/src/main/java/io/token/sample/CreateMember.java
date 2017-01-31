5ab0f252e12b06c9d8349558f8a94290d9e49091
Maxim Khutornenko <maxim.khutornenko@token.io>
2017-01-30 17:52:14 -0800

    Moving sdk into a separate module and introducing samples module.

---
+++ samples/src/main/java/io/token/sample/CreateMember.java
@@ -0,0 +1,26 @@
+package io.token.sample;
+
+import com.google.common.net.HostAndPort;
+import io.token.Member;
+import io.token.Token;
+import io.token.util.Util;
+
+import java.time.Duration;
+
+/**
+ * Create a new Token member record.
+ */
+public class CreateMember {
+    Member createMember() {
+        // Initialize Token SDK instance.
+        Token sdk = Token.builder()
+                .hostName(HostAndPort.fromHost("api-grpc.token.io").getHostText())
+                .port(443)
+                .timeout(Duration.ofSeconds(15))
+                .useSsl(true)
+                .build();
+
+        // Create a member account with a random username.
+        return sdk.createMember("username-" + Util.generateNonce());
+    }
+}
