fe0517fd9d8ac61272c92a6e51b78c246a12832a
Alexey Kalinichenko <alexey@token.io>
2017-02-02 10:02:43 -0800

    Additional tests to verify that errors are sent back for transfer.

--- sdk/src/test/java/io/token/TransferRedemptionTest.java
+++ sdk/src/test/java/io/token/TransferRedemptionTest.java
@@ -4,8 +4,10 @@ import static io.token.asserts.TransferAssertion.assertThat;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.token.proto.PagedList;
+import io.token.proto.common.money.MoneyProtos.Money;
 import io.token.proto.common.security.SecurityProtos.Key;
 import io.token.proto.common.token.TokenProtos.Token;
+import io.token.proto.common.transaction.TransactionProtos.TransactionStatus;
 import io.token.proto.common.transfer.TransferProtos.Transfer;
 
 import org.junit.Rule;
@@ -20,11 +22,12 @@ public class TransferRedemptionTest {
 
     @Test
     public void redeemToken() {
-        Token token = token();
+        Token token = token(100.0);
         token = payer.endorseToken(token, Key.Level.STANDARD).getToken();
 
         Transfer transfer = payee.redeemToken(token);
         assertThat(transfer)
+                .isSuccessful()
                 .hasNoAmount()
                 .hasNSignatures(2)
                 .isSignedBy(payee, Key.Level.LOW);
@@ -32,11 +35,12 @@ public class TransferRedemptionTest {
 
     @Test
     public void redeemToken_withParams() {
-        Token token = token();
+        Token token = token(100.0);
         token = payer.endorseToken(token, Key.Level.STANDARD).getToken();
 
         Transfer transfer = payee.redeemToken(token, 99.0, "USD", "transfer description");
         assertThat(transfer)
+                .isSuccessful()
                 .hasAmount(99.0)
                 .hasCurrency("USD")
                 .hasNSignatures(2)
@@ -44,18 +48,37 @@ public class TransferRedemptionTest {
     }
 
     @Test
+    public void redeemToken_failure() {
+        Money balance = payerAccount.getBalance();
+        double amount = Double.parseDouble(balance.getValue()) + 1; // 1 over the limit.
+
+        Token token = token(amount);
+        token = payer.endorseToken(token, Key.Level.STANDARD).getToken();
+
+        Transfer transfer = payee.redeemToken(token, amount, "USD", "transfer description");
+        assertThat(transfer)
+                .hasStatus(TransactionStatus.FAILURE_INSUFFICIENT_FUNDS)
+                .hasAmount(amount)
+                .hasCurrency("USD")
+                .hasNSignatures(2)
+                .isSignedBy(payee, Key.Level.LOW);
+    }
+
+    @Test
     public void getTransfer() {
-        Token token = token();
+        Token token = token(100.0);
         token = payer.endorseToken(token, Key.Level.STANDARD).getToken();
 
         Transfer transfer = payee.redeemToken(token);
         Transfer lookedUp = payer.getTransfer(transfer.getId());
-        assertThat(lookedUp).isEqualTo(transfer);
+        assertThat(lookedUp)
+                .isSuccessful()
+                .isEqualTo(transfer);
     }
 
     @Test
     public void getTransfers() {
-        Token token = token();
+        Token token = token(100.0);
         token = payer.endorseToken(token, Key.Level.STANDARD).getToken();
 
         Transfer transfer1 = payee.redeemToken(token, 10.0, "USD", "first");
@@ -63,14 +86,17 @@ public class TransferRedemptionTest {
         Transfer transfer3 = payee.redeemToken(token, 70.0, "USD", "third");
 
         assertThat(transfer1)
+                .isSuccessful()
                 .hasAmount(10.0)
                 .hasCurrency("USD")
                 .hasDescription("first");
         assertThat(transfer2)
+                .isSuccessful()
                 .hasAmount(20.0)
                 .hasCurrency("USD")
                 .hasDescription("second");
         assertThat(transfer3)
+                .isSuccessful()
                 .hasAmount(70.0)
                 .hasCurrency("USD")
                 .hasDescription("third");
@@ -85,9 +111,9 @@ public class TransferRedemptionTest {
         assertThat(lookedUp.getOffset()).isNotEmpty();
     }
 
-    private Token token() {
+    private Token token(double amount) {
         return payer.createToken(
-                100.0,
+                amount,
                 "USD",
                 payerAccount.id(),
                 payee.firstUsername(),
