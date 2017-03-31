dd10aab39b70f169ee3a48e28c7b4f5411af586b
Maxim Khutornenko <maxim.khutornenko@token.io>
2017-03-31 10:36:04 -0700

    Switch bank calls to REST api

---
+++ sdk/src/test/java/io/token/BankClientApi.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) 2017 Token, Inc.
+ * <p>
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+package io.token;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+/**
+ * Wraps around HTTP REST API for the fake bank client.
+ */
+public interface BankClientApi {
+    @PUT("/clients")
+    Call<String> addClient(@Body String request);
+
+    @PUT("/clients/{client_id}/accounts")
+    Call<String> addAccount(@Body String request, @Path("client_id") String clientId);
+
+    @PUT("/clients/{client_id}/link-accounts")
+    Call<String> authorizeLinkAccounts(@Body String request, @Path("client_id") String clientId);
+}
