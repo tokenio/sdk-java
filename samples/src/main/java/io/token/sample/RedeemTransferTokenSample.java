d91618b7708074bb46dbc3c5ec4889569fbc5460
Larry Hosken <larry.hosken@token.io>
2017-06-05 17:34:16 -0700

    divvy up into more files

--- samples/src/main/java/io/token/sample/RedeemTransferTokenSample.java
+++ samples/src/main/java/io/token/sample/RedeemTransferTokenSample.java
@@ -2,12 +2,8 @@ package io.token.sample;
 
 import io.token.Destinations;
 import io.token.Member;
-import io.token.proto.common.blob.BlobProtos.Attachment;
-import io.token.proto.common.blob.BlobProtos.Blob;
 import io.token.proto.common.token.TokenProtos.Token;
 import io.token.proto.common.transfer.TransferProtos.Transfer;
-
-import java.util.List;
 
 /**
  * Redeems a transfer token.
@@ -34,38 +30,5 @@ public final class RedeemTransferTokenSample {
                 Destinations.token(payee.memberId(), accountId));
 
         return transfer;
-    }
-
-    private static void showImage(String name, String mimeType, byte[] data) {
-        // no-op fake to make example look plausible
-    }
-
-    /**
-     * Show how to download attachment data from a token
-     * @param payee Payee member.
-     * @param tokenId Token with attachments we want to show
-     */
-    public static void displayAttachmentFromTransferToken(
-            Member payee,
-            String tokenId) {
-        // Retrieve a transfer token to redeem.
-        Token transferToken = payee.getToken(tokenId);
-
-        List<Attachment> attachments = transferToken
-                .getPayload()
-                .getTransfer()
-                .getAttachmentsList();
-        for (Attachment attachment : attachments) {
-            // Attachment has some metadata (name, type) but not the "file" contents.
-            if (attachment.getType().startsWith("image/")) {
-                // Download the contents for the attachment[s] we want:
-                Blob blob = payee.getTokenBlob(tokenId, attachment.getBlobId());
-                // Use the attachment data.
-                showImage(
-                        blob.getPayload().getName(),  // "invoice.jpg"
-                        blob.getPayload().getType(),  // MIME, e.g., "image/jpeg"
-                        blob.getPayload().getData().toByteArray()); // byte[] of contents
-            }
-        }
     }
 }
