d99fb7b80246c5eea34b17c903b8293930e6d46a
Larry Hosken <lahosken@gmail.com>
2017-08-03 09:27:46 -0700

    sample code for getTransaction, getTransactions (#1)

---
+++ samples/src/main/java/io/token/sample/GetTransactionsSample.java
@@ -0,0 +1,59 @@
+package io.token.sample;
+
+import io.token.Account;
+import io.token.Member;
+import io.token.proto.common.transaction.TransactionProtos.Transaction;
+import io.token.proto.common.transaction.TransactionProtos.TransactionStatus;
+import io.token.proto.common.transaction.TransactionProtos.TransactionType;
+import io.token.proto.common.transfer.TransferProtos.Transfer;
+
+import java.util.List;
+
+/**
+ * Redeems a transfer token.
+ */
+public final class GetTransactionsSample {
+    /**
+     * Illustrate Member.getTransactions
+     *
+     * @param payer payer Token member
+     */
+    public static void getTransactionsSample(Member payer) {
+        List<Account> accounts = payer.getAccounts();
+        String accountId = accounts.get(0).id();
+
+        for (Transaction transaction :
+                payer.getTransactions(accountId, "0", 10).getList()) {
+            displayTransaction(
+                    transaction.getAmount().getCurrency(),
+                    transaction.getAmount().getValue(),
+                    transaction.getType(), // debit or credit
+                    transaction.getStatus());
+        }
+    }
+
+    /**
+     * Illustrate Member.getTransaction
+     *
+     * @param payer payer Token member
+     * @param transfer recently-completed transfer
+     * @return a Transaction
+     */
+    public static Transaction getTransactionSample(
+            Member payer,
+            Transfer transfer) {
+        List<Account> accounts = payer.getAccounts();
+        String accountId = accounts.get(0).id();
+
+        String transactionId = transfer.getTransactionId();
+        Transaction transaction = payer.getTransaction(accountId, transactionId);
+        return transaction;
+    }
+
+    private static void displayTransaction(
+            String currency,
+            String value,
+            TransactionType debitOrCredit,
+            TransactionStatus status) {
+    }
+}
