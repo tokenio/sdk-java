83a7e8f2484acdfa115d8ba00b883ccf372bacf0
Larry Hosken <lahosken@gmail.com>
2017-08-07 11:41:42 -0700

    samples calling many TransferTokenBuilder methods (#5)

--- samples/src/main/java/io/token/sample/CreateAndEndorseTransferTokenWithAttachmentSample.java
+++ samples/src/main/java/io/token/sample/CreateAndEndorseTransferTokenWithAttachmentSample.java
@@ -1,6 +1,10 @@
 package io.token.sample;
 
+import static io.token.proto.common.blob.BlobProtos.Blob.AccessMode.DEFAULT;
+
 import io.token.Member;
+import io.token.proto.common.blob.BlobProtos.Attachment;
+import io.token.proto.common.blob.BlobProtos.Blob;
 import io.token.proto.common.security.SecurityProtos.Key;
 import io.token.proto.common.token.TokenProtos.Token;
 
@@ -15,6 +19,7 @@ public final class CreateAndEndorseTransferTokenWithAttachmentSample {
 
     /**
      * Create a new transfer token, including an attached image file.
+     *
      * @param payer Payer member token
      * @param payeeUsername Token member username of payee
      * @return Token
@@ -42,6 +47,45 @@ public final class CreateAndEndorseTransferTokenWithAttachmentSample {
         transferToken = payer.endorseToken(
                 transferToken,
                 Key.Level.STANDARD).getToken();
+
+        return transferToken;
+    }
+
+    /**
+     * Create a new transfer token, including an attached image file.
+     *
+     * @param payer Payer member token
+     * @param payeeId Token member username of payee
+     * @return Token
+     */
+    public static Token createTransferTokenWithExistingAttachment(
+            Member payer,
+            String payeeId) {
+
+        Attachment attachment = payer.createBlob(
+                payeeId,
+                "image/jpeg",
+                "invoice.jpg",
+                loadImageByteArray("invoice.jpg"),
+                DEFAULT);
+
+        // Create a transfer token.
+        Token transferToken =
+                payer.createTransferToken(
+                        100.0, // amount
+                        "EUR")  // currency
+                        .setAccountId(payer.getAccounts().get(0).id()) // source account
+                        .setRedeemerMemberId(payeeId)
+                        .setDescription("Invoice payment") // optional description
+                        .addAttachment(attachment)
+                        .execute();
+
+        // Payer endorses a token to a payee by signing it with her secure private key.
+        transferToken = payer.endorseToken(
+                transferToken,
+                Key.Level.STANDARD).getToken();
+
+        Blob copyBlob = payer.getBlob(attachment.getBlobId());
 
         return transferToken;
     }
--- samples/src/test/java/io/token/sample/CreateAndEndorseTransferTokenSampleTest.java
+++ samples/src/test/java/io/token/sample/CreateAndEndorseTransferTokenSampleTest.java
@@ -2,6 +2,9 @@ package io.token.sample;
 
 import static io.token.TokenIO.TokenCluster.DEVELOPMENT;
 import static io.token.sample.CreateAndEndorseTransferTokenSample.createTransferToken;
+import static io.token.sample.CreateAndEndorseTransferTokenSample.createTransferTokenFromAuth;
+import static io.token.sample.CreateAndEndorseTransferTokenSample.createTransferTokenToDestination;
+import static io.token.sample.CreateAndEndorseTransferTokenSample.createTransferTokenWithOtherOptions;
 import static io.token.sample.TestUtil.newUserName;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,6 +24,43 @@ public class CreateAndEndorseTransferTokenSampleTest {
             LinkMemberAndBankSample.linkBankAccounts(payer);
 
             Token token = createTransferToken(payer, payee.firstUsername());
+            assertThat(token).isNotNull();
+        }
+    }
+
+    @Test
+    public void createPaymentTokenWithOtherOptionsTest() {
+        try (TokenIO tokenIO = TokenIO.create(DEVELOPMENT)) {
+            Member payer = tokenIO.createMember(newUserName());
+            Member payee = tokenIO.createMember(newUserName());
+
+            LinkMemberAndBankSample.linkBankAccounts(payer);
+
+            Token token = createTransferTokenWithOtherOptions(payer, payee.memberId());
+            assertThat(token).isNotNull();
+        }
+    }
+
+    @Test
+    public void createPaymentTokenFromAuthTest() {
+        try (TokenIO tokenIO = TokenIO.create(DEVELOPMENT)) {
+            Member payer = tokenIO.createMember(newUserName());
+            Member payee = tokenIO.createMember(newUserName());
+
+            Token token = createTransferTokenFromAuth(payer, payee.firstUsername());
+            assertThat(token).isNotNull();
+        }
+    }
+
+    @Test
+    public void createPaymentTokenToDestinationTest() {
+        try (TokenIO tokenIO = TokenIO.create(DEVELOPMENT)) {
+            Member payer = tokenIO.createMember(newUserName());
+            Member payee = tokenIO.createMember(newUserName());
+
+            LinkMemberAndBankSample.linkBankAccounts(payer);
+
+            Token token = createTransferTokenToDestination(payer, payee.firstUsername());
             assertThat(token).isNotNull();
         }
     }
