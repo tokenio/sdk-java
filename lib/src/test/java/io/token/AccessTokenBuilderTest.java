5ec393d444ce5e0a9375806aa8af1596e28694e9
Colin Man <colin@token.io>
2017-08-10 16:39:43 -0700

    Fix lib tests

--- lib/src/test/java/io/token/AccessTokenBuilderTest.java
+++ lib/src/test/java/io/token/AccessTokenBuilderTest.java
@@ -1,22 +1,28 @@
 package io.token;
 
+import static io.token.proto.common.alias.AliasProtos.Alias.Type.EMAIL;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import io.token.proto.common.alias.AliasProtos.Alias;
 import io.token.proto.common.token.TokenProtos;
 
 import org.junit.Test;
 
 public class AccessTokenBuilderTest {
+    private static Alias alias = Alias.newBuilder()
+            .setValue("alias@token.io")
+            .setType(EMAIL)
+            .build();
 
     @Test(expected = IllegalArgumentException.class)
     public void throwsOnMissingFrom() {
-        AccessTokenBuilder.create("alias").build();
+        AccessTokenBuilder.create(alias).build();
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void throwsOnMissingResource() {
         AccessTokenBuilder
-                .create("alias")
+                .create(alias)
                 .from("member")
                 .build();
     }
@@ -24,7 +30,7 @@ public class AccessTokenBuilderTest {
     @Test
     public void allValidationsPass() {
         TokenProtos.TokenPayload payload = AccessTokenBuilder
-                .create("alias")
+                .create(alias)
                 .from("member")
                 .forAllAccounts()
                 .build();
