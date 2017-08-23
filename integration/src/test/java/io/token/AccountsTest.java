4c0a48ad0efaa86560cf4eeafafa8fadba2b90ec
brianhillsley <brian.hillsley@token.io>
2017-08-23 15:48:59 -0700

    Revised AccountsTest.java test cases for get/set default bank

--- integration/src/test/java/io/token/AccountsTest.java
+++ integration/src/test/java/io/token/AccountsTest.java
@@ -1,13 +1,14 @@
 package io.token;
 
-import static io.token.testing.sample.Sample.string;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.token.common.LinkedAccount;
 import io.token.common.TokenRule;
+import io.token.proto.common.bank.BankProtos.Bank;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,7 +20,9 @@ public class AccountsTest {
     @Test
     public void linkAccounts() {
         LinkedAccount account = rule.linkedAccount();
-        assertThat(account.getMember().getAccounts()).containsExactly(account.getAccount());
+        assertThat(account
+                .getMember()
+                .getAccounts()).containsExactly(account.getAccount());
 
         account.getMember().unlinkAccounts(singletonList(account.getId()));
         assertThat(account.getMember().getAccounts()).isEmpty();
@@ -28,17 +31,23 @@ public class AccountsTest {
     @Test
     public void linkAccounts_relinking() {
         LinkedAccount account = rule.linkedAccount();
-        assertThat(account.getMember().getAccounts()).containsExactly(account.getAccount());
+        assertThat(account
+                .getMember()
+                .getAccounts()).containsExactly(account.getAccount());
 
         rule.relinkAccount(account);
         rule.relinkAccount(account);
-        assertThat(account.getMember().getAccounts()).containsExactly(account.getAccount());
+        assertThat(account
+                .getMember()
+                .getAccounts()).containsExactly(account.getAccount());
     }
 
     @Test
     public void getAccounts() {
         LinkedAccount account = rule.linkedAccount();
-        assertThat(account.getMember().getAccounts()).containsExactly(account.getAccount());
+        assertThat(account
+                .getMember()
+                .getAccounts()).containsExactly(account.getAccount());
 
         List<Account> accounts = account.getMember().getAccounts();
         assertThat(accounts).containsExactly(account.getAccount());
@@ -57,8 +66,11 @@ public class AccountsTest {
     @Test
     public void getAccount() {
         LinkedAccount account = rule.linkedAccount();
-        assertThat(account.getMember().getAccounts()).containsExactly(account.getAccount());
-        assertThat(account.getMember().getAccount(account.getId())).isEqualTo(account.getAccount());
+        assertThat(account
+                .getMember()
+                .getAccounts()).containsExactly(account.getAccount());
+        assertThat(account.getMember().getAccount(account.getId())).isEqualTo(
+                account.getAccount());
     }
 
     @Test
@@ -70,12 +82,25 @@ public class AccountsTest {
 
     @Test
     public void setDefaultBank() {
-        String bankId = rule.linkedAccount().getMember().getBanks().get(0).getId();
+        String bankId = rule
+                .linkedAccount()
+                .getMember()
+                .getBanks()
+                .get(0)
+                .getId();
         rule.linkedAccount().getMember().setDefaultBank(bankId);
     }
 
     @Test
     public void getDefaultBank() {
-        assertThat(rule.linkedAccount().getMember().getDefaultBank()).isNotEmpty();
+        List<String> bankIds = rule
+                .linkedAccount()
+                .getMember()
+                .getBanks()
+                .stream()
+                .map(Bank::getId)
+                .collect(Collectors.toList());
+        assertThat(rule.linkedAccount().getMember().getDefaultBank()).isIn(
+                bankIds);
     }
 }
