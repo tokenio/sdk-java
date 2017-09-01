dc1b6bfad9b16b3ea2565c96ac1c6ad4cda429f8
Larry Hosken <lahosken@gmail.com>
2017-09-01 14:26:05 -0700

    sample code: show how to persist keys (#33)

--- samples/src/main/java/io/token/sample/CreateMemberSample.java
+++ samples/src/main/java/io/token/sample/CreateMemberSample.java
@@ -6,6 +6,12 @@ import static io.token.proto.common.testing.Sample.alias;
 import io.token.Member;
 import io.token.TokenIO;
 import io.token.proto.common.alias.AliasProtos.Alias;
+import io.token.security.UnsecuredFileSystemKeyStore;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public final class CreateMemberSample {
     /**
@@ -16,16 +22,22 @@ public final class CreateMemberSample {
     public static Member createMember() {
         // Create the client, which communicates with
         // the Token cloud.
-        TokenIO tokenIO = TokenIO.builder()
-                .connectTo(SANDBOX)
-                .build();
+        try {
+            Path keys = Files.createDirectories(Paths.get("./keys"));
+            TokenIO tokenIO = TokenIO.builder()
+                    .withKeyStore(new UnsecuredFileSystemKeyStore(keys.toFile()))
+                    .connectTo(SANDBOX)
+                    .build();
 
-        // The alias() method generates a random-nonsense-string alias.
-        // "name@token.io" would be more typical than a random string.
-        // But if we run this code with the same alias twice,
-        // the 2nd time it will fail because the name is taken.
-        Alias alias = alias();
+            // The alias() method generates a random-nonsense-string alias.
+            // "name@token.io" would be more typical than a random string.
+            // But if we run this code with the same alias twice,
+            // the 2nd time it will fail because the name is taken.
+            Alias alias = alias();
 
-        return tokenIO.createMember(alias);
+            return tokenIO.createMember(alias);
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
     }
 }
