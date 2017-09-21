e41af5b26b6225e9c22df6e647e0e643a33f6dc8
Larry Hosken <lahosken@gmail.com>
2017-09-21 07:46:25 -0700

    sample code for Member.getNotifications (+ test to exercise it) (#44)

---
+++ samples/src/test/java/io/token/sample/PollNotificationsSampleTest.java
@@ -0,0 +1,36 @@
+package io.token.sample;
+
+import static io.token.TokenIO.TokenCluster.DEVELOPMENT;
+import static io.token.sample.TestUtil.newAlias;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.token.Member;
+import io.token.TokenIO;
+import io.token.proto.common.notification.NotificationProtos.Notification;
+import io.token.proto.common.notification.NotificationProtos.NotifyStatus;
+
+import java.util.Optional;
+
+import org.junit.Test;
+
+public class PollNotificationsSampleTest {
+
+    @Test
+    public void notifyPaymentRequestSampleTest() {
+        try (TokenIO tokenIO = TokenIO.create(DEVELOPMENT)) {
+            Member payer = PollNotificationsSample.createMember(tokenIO);
+            Member payee = tokenIO.createMember(newAlias());
+
+            LinkMemberAndBankSample.linkBankAccounts(payer);
+            LinkMemberAndBankSample.linkBankAccounts(payee);
+
+            NotifyStatus status = NotifyPaymentRequestSample.notifyPaymentRequest(
+                    tokenIO,
+                    payee,
+                    payer.firstAlias());
+            assertThat(status).isNotNull();
+            Optional<Notification> notification = PollNotificationsSample.poll(payer);
+            assertThat(notification.isPresent()).isTrue();
+        }
+    }
+}
