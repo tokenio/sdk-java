1face0eb5babf0453ea0f1d631b67ff2a96e7c9f
VKCR <vivek.claver@gmail.com>
2017-10-10 16:09:16 -0700

    adding developer key related unit tests

---
+++ samples/src/test/java/io/token/sample/ValidateDeveloperKey.java
@@ -0,0 +1,37 @@
+package io.token.sample;
+
+import static io.token.TokenIO.TokenCluster.DEVELOPMENT;
+import static io.token.sample.TestUtil.newAlias;
+
+import io.grpc.StatusRuntimeException;
+import io.token.Member;
+import io.token.TokenIO;
+
+import org.junit.Test;
+
+public class ValidateDeveloperKey {
+    @Test
+    public void validKey() {
+        try (TokenIO tokenIO = TokenIO.create(
+                DEVELOPMENT,
+                "4qY7lqQw8NOl9gng0ZHgT4xdiDqxqoGVutuZwrUYQsI")) {
+            Member member = tokenIO.createMember(newAlias());
+        }
+    }
+
+    @Test(expected = StatusRuntimeException.class)
+    public void invalidKey() {
+        TokenIO tokenIO = TokenIO.create(
+                DEVELOPMENT,
+                "0");
+        Member member = tokenIO.createMember(newAlias());
+    }
+
+    @Test(expected = StatusRuntimeException.class)
+    public void noKeyProvided() {
+        TokenIO tokenIO = TokenIO.create(
+                DEVELOPMENT,
+                null
+        );
+    }
+}
