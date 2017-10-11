c69e84ee4b31a7fa0506cecc62fc2751b49edac1
VKCR <vivek.claver@gmail.com>
2017-10-11 14:44:26 -0700

    implementing comments

--- samples/src/test/java/io/token/common/Constants.java
+++ samples/src/test/java/io/token/common/Constants.java
@@ -1,7 +1,7 @@
 package io.token.common;
 
 public class Constants {
-    public static String DEV_KEY = "4qY7lqQw8NOl9gng0ZHgT4xdiDqxqoGVutuZwrUYQsI";
+    public static final String DEV_KEY = "4qY7lqQw8NOl9gng0ZHgT4xdiDqxqoGVutuZwrUYQsI";
 
     private Constants() {}
 }
