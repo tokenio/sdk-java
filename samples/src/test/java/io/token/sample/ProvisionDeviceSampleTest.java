19749fee6ed9c88d73c965204fe0fa15f5ef0234
Larry Hosken <larry.hosken@token.io>
2017-10-19 15:26:15 -0700

    blow the dust off of this

--- samples/src/test/java/io/token/sample/ProvisionDeviceSampleTest.java
+++ samples/src/test/java/io/token/sample/ProvisionDeviceSampleTest.java
@@ -1,7 +1,5 @@
 package io.token.sample;
 
-import static io.token.TokenIO.TokenCluster.DEVELOPMENT;
-import static io.token.sample.TestUtil.newAlias;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.token.Member;
@@ -11,19 +9,16 @@ import io.token.proto.common.security.SecurityProtos.Key;
 
 import org.junit.Test;
 
-
 public class ProvisionDeviceSampleTest {
     @Test
     public void provisionDevice() {
-        try (TokenIO remoteDevice = TokenIO.create(DEVELOPMENT)) {
-            Alias alias = newAlias();
+        try (TokenIO remoteDevice = TestUtil.createClient()) {
+            Alias alias = TestUtil.randomAlias();
             Member remoteMember = remoteDevice.createMember(alias);
             remoteMember.subscribeToNotifications("iron");
 
-            TokenIO localDeviceClient = TokenIO.create(DEVELOPMENT);
-
+            TokenIO localDeviceClient = TestUtil.createClient();
             Key key = ProvisionDeviceSample.provisionDevice(localDeviceClient, alias);
-
             remoteMember.approveKey(key);
 
             Member local = ProvisionDeviceSample.useProvisionedDevice(localDeviceClient, alias);
