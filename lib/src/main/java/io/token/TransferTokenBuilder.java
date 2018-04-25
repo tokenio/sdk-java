62d0c0438bed34dd5eed9fc6179b31d1c06269c7
Sam Holeman <sam.holeman@token.io>
2018-04-25 12:17:33 -0700

    PL-1550: Add acting_as to token builders (#143)

--- lib/src/main/java/io/token/TransferTokenBuilder.java
+++ lib/src/main/java/io/token/TransferTokenBuilder.java
@@ -43,6 +43,7 @@ import io.token.proto.common.pricing.PricingProtos.Pricing;
 import io.token.proto.common.token.TokenProtos;
 import io.token.proto.common.token.TokenProtos.Token;
 import io.token.proto.common.token.TokenProtos.TokenPayload;
+import io.token.proto.common.token.TokenProtos.TokenPayload.ActingAs;
 import io.token.proto.common.token.TokenProtos.TransferBody;
 import io.token.proto.common.transferinstructions.TransferInstructionsProtos.PurposeOfPayment;
 import io.token.proto.common.transferinstructions.TransferInstructionsProtos.TransferEndpoint;
@@ -351,6 +352,17 @@ public final class TransferTokenBuilder {
                 .getInstructionsBuilder()
                 .getMetadataBuilder()
                 .setTransferPurpose(purposeOfPayment);
+        return this;
+    }
+
+    /**
+     * Sets acting as on the token.
+     *
+     * @param actingAs entity the redeemer is acting on behalf of
+     * @return builder
+     */
+    public TransferTokenBuilder setActingAs(ActingAs actingAs) {
+        payload.setActingAs(actingAs);
         return this;
     }
 
