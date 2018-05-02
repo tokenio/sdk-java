1ebc5c52309b4cf439563d700e82ab6443883953
VKCR <vivek.claver@gmail.com>
2018-05-02 11:36:39 -0700

    nit

--- lib/src/main/java/io/token/TransferTokenBuilder.java
+++ lib/src/main/java/io/token/TransferTokenBuilder.java
@@ -97,7 +97,7 @@ public final class TransferTokenBuilder {
                     .build());
 
             Alias alias = member.firstAlias().blockingSingle();
-            if (alias != Alias.getDefaultInstance()) {
+            if (!alias.equals(Alias.getDefaultInstance())) {
                 payload.getFromBuilder()
                         .setAlias(alias);
             }
