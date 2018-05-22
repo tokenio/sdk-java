961a303dfa2a6b0778468694a385852f460c667e
Gianluca Pane <Gianluca.Pane@token.io>
2018-05-22 16:05:02 -0700

    also override new method

--- android/src/main/java/io/token/browser/TokenBrowserActivity.java
+++ android/src/main/java/io/token/browser/TokenBrowserActivity.java
@@ -1,12 +1,13 @@
 package io.token.browser;
 
-import android.annotation.TargetApi;
+
 import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.RequiresApi;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -44,6 +45,16 @@ public class TokenBrowserActivity extends Activity {
                 Bundle data = new Bundle(2);
                 data.putString(MSG_KEY_SID, sessionId);
                 data.putString(MSG_KEY_URL, url);
+                messenger.send(MSG_ON_URL, data);
+                return true;
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                Bundle data = new Bundle(2);
+                data.putString(MSG_KEY_SID, sessionId);
+                data.putString(MSG_KEY_URL, request.getUrl().toString());
                 messenger.send(MSG_ON_URL, data);
                 return true;
             }
