2f25b23946db64bcedcfb21c2cc457bc61c26186
Gianluca Pane <Gianluca.Pane@token.io>
2019-01-10 17:13:24 -0800

    copyright year = 2019

--- common/src/test/java/io/token/util/TestClock.java
+++ common/src/test/java/io/token/util/TestClock.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2018 Token, Inc.
+ * Copyright (c) 2019 Token, Inc.
  * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
