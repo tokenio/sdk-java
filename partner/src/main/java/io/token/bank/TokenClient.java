d19ea8a2a62655df68f8596e717fc57f16a532d9
Gianluca Pane <Gianluca.Pane@token.io>
2019-01-24 10:17:57 -0800

    remove bank-sample RD-1805: partner module

---
+++ partner/src/main/java/io/token/bank/TokenClient.java
@@ -0,0 +1,168 @@
+/**
+ * Copyright (c) 2019 Token, Inc.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package io.token.bank;
+
+import static io.token.TokenClient.TokenCluster.SANDBOX;
+import static io.token.proto.common.member.MemberProtos.CreateMemberType.BUSINESS;
+
+import io.grpc.ManagedChannel;
+import io.grpc.Metadata;
+
+import io.reactivex.Completable;
+import io.reactivex.Observable;
+import io.reactivex.functions.Function;
+import io.token.bank.rpc.Client;
+import io.token.bank.rpc.ClientFactory;
+import io.token.proto.common.alias.AliasProtos.Alias;
+import io.token.proto.common.member.MemberProtos.CreateMemberType;
+import io.token.rpc.client.RpcChannelFactory;
+import io.token.security.CryptoEngine;
+import io.token.security.CryptoEngineFactory;
+import io.token.security.InMemoryKeyStore;
+import io.token.security.TokenCryptoEngineFactory;
+
+public class TokenClient extends io.token.TokenClient {
+    /**
+     * Creates an instance of a Token SDK.
+     *
+     * @param channel GRPC channel
+     * @param cryptoFactory crypto factory instance
+     * @param tokenCluster token cluster
+     */
+    TokenClient(
+            ManagedChannel channel,
+            CryptoEngineFactory cryptoFactory,
+            TokenCluster tokenCluster) {
+        super(channel, cryptoFactory, tokenCluster);
+
+    }
+
+    /**
+     * Creates a new {@link Builder} instance that is used to configure and
+     * build a {@link TokenClient} instance.
+     *
+     * @return builder
+     */
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * Creates a new instance of {@link TokenClient} that's configured to use
+     * the specified environment.
+     *
+     * @param cluster token cluster to connect to
+     * @param developerKey developer key
+     * @return {@link TokenClient} instance
+     */
+    public static TokenClient create(TokenCluster cluster, String developerKey) {
+        return TokenClient.builder()
+                .connectTo(cluster)
+                .devKey(developerKey)
+                .build();
+    }
+
+    /**
+     * Creates a new Token member with a set of auto-generated keys, an alias, and member type.
+     *
+     * @param alias nullable member alias to use, must be unique. If null, then no alias will
+     *     be created with the member.
+     * @param memberType the type of member to register
+     * @return newly created member
+     */
+    public Observable<Member> createMember(
+            final Alias alias,
+            final CreateMemberType memberType) {
+        return createMemberImpl(alias, memberType)
+                .map(new Function<io.token.Member, Member>() {
+                    @Override
+                    public Member apply(io.token.Member mem) {
+                        CryptoEngine crypto = cryptoFactory.create(mem.memberId());
+                        final Client client = ClientFactory.authenticated(
+                                channel,
+                                mem.memberId(),
+                                crypto);
+                        return new Member(mem, client);
+                    }
+                });
+    }
+
+    /**
+     * Creates a new business-use Token member with a set of auto-generated keys and and an alias.
+     *
+     * @param alias alias to associate with member
+     * @return newly created member
+     */
+    public Observable<Member> createMember(Alias alias) {
+        return createMember(alias, BUSINESS);
+    }
+
+    /**
+     * Creates a new Token member with a set of auto-generated keys, an alias, and member type.
+     *
+     * @param alias nullable member alias to use, must be unique. If null, then no alias will
+     *     be created with the member.
+     * @param memberType the type of member to register
+     * @return newly created member
+     */
+    public Member createMemberBlocking(
+            final Alias alias,
+            final CreateMemberType memberType) {
+        return createMember(alias, memberType).blockingSingle();
+    }
+
+    /**
+     * Creates a new business-use Token member with a set of auto-generated keys and and an alias.
+     *
+     * @param alias alias to associate with member
+     * @return newly created member
+     */
+    public Member createMemberBlocking(Alias alias) {
+        return createMember(alias).blockingSingle();
+    }
+
+    public static final class Builder extends io.token.TokenClient.Builder<Builder> {
+        /**
+         * Creates new builder instance with the defaults initialized.
+         */
+        public Builder() {
+            super();
+        }
+
+        @Override
+        public TokenClient build() {
+            Metadata headers = getHeaders();
+            return new TokenClient(
+                    RpcChannelFactory
+                            .builder(hostName, port, useSsl)
+                            .withTimeout(timeoutMs)
+                            .withMetadata(headers)
+                            .withClientSsl(sslConfig)
+                            .build(),
+                    cryptoEngine != null
+                            ? cryptoEngine
+                            : new TokenCryptoEngineFactory(new InMemoryKeyStore()),
+                    tokenCluster == null ? SANDBOX : tokenCluster);
+        }
+    }
+}
