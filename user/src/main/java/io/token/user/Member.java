da2c5d81986ecfbc7957d5b7e4c71249571aee03
Gianluca Pane <31416110+gianluca-token@users.noreply.github.com>
2019-06-24 18:39:45 -0700

    RD-2283: request BALANCES and TRANSACTIONS access on linking (#310)
    Copy full transfer instructions from token request to token payload

--- user/src/main/java/io/token/user/Member.java
+++ user/src/main/java/io/token/user/Member.java
@@ -1307,8 +1307,9 @@ public class Member extends io.token.Member {
                                                     }
                                                 });
                                 String linkingUrl = bankInfo.getBankLinkingUri();
-                                String url = String.format(
-                                        "%s&redirect_uri=%s",
+                                String url = String.format("%s&redirect_uri=%s"
+                                                // request BALANCE and TRANSACTION access on linking
+                                                + "&resource=BALANCES&resource=TRANSACTIONS",
                                         linkingUrl,
                                         URLEncoder.encode(callbackUrl, "UTF-8"));
                                 browser.goTo(new URL(url));
