e0d24d420d1b13e3d85c2468432aa94adf5f1bfb
Prateek Gupta <prateek.gupta@token.io>
2019-09-16 15:16:20 -0700

    Moved Cross border sample to Token request samples

--- tpp-sample/src/main/java/io/token/sample/StoreAndRetrieveTokenRequestSample.java
+++ tpp-sample/src/main/java/io/token/sample/StoreAndRetrieveTokenRequestSample.java
@@ -4,10 +4,14 @@ import static io.token.proto.common.token.TokenProtos.TokenRequestPayload.Access
 import static io.token.proto.common.token.TokenProtos.TokenRequestPayload.AccessBody.ResourceType.BALANCES;
 import static io.token.util.Util.generateNonce;
 
+import io.token.proto.common.alias.AliasProtos;
 import io.token.proto.common.alias.AliasProtos.Alias;
+import io.token.proto.common.transferinstructions.TransferInstructionsProtos.TransferDestination;
 import io.token.tokenrequest.TokenRequest;
 import io.token.tpp.Member;
 import io.token.tpp.TokenClient;
+
+import java.util.List;
 
 /**
  * Stores and retrieves a token request.
@@ -26,15 +30,48 @@ public final class StoreAndRetrieveTokenRequestSample {
                 .setDescription("Book purchase") // optional description
                 .setRedirectUrl("https://token.io/callback") // callback URL
                 .setFromAlias(Alias.newBuilder()
-                                .setValue("payer-alias@token.io") // user alias
-                                .setType(Alias.Type.EMAIL)
-                                .build())
+                        .setValue("payer-alias@token.io") // user alias
+                        .setType(Alias.Type.EMAIL)
+                        .build())
                 .setBankId("iron") // bank ID
                 .setCsrfToken(generateNonce()) // nonce for CSRF check
                 .build();
 
         // Store token request
         return payee.storeTokenRequestBlocking(request);
+    }
+
+    /**
+     * Stores a transfer token without setting Transfer Destinations.
+     *
+     * @param payee Payee Token member (the member requesting the transfer token be created)
+     * @param setTransferDestinationsCallback callback url.
+     * @return token request id
+     */
+    public static String storeTransferTokenRequest(
+            Member payee,
+            String setTransferDestinationsCallback) {
+
+        TokenRequest request = TokenRequest.transferTokenRequestBuilder(100, "EUR")
+                .setToMemberId(payee.memberId())
+                .setDescription("Book purchase")
+                // This TPP provided url gets called by Token after the user selects bank and
+                // country on the Token web app.
+                .setSetTransferDestinationsUrl(setTransferDestinationsCallback)
+                // This TPP provided Redirect URL gets called after Token is ready
+                // for redemption.
+                .setRedirectUrl("https://tpp-sample.com/callback")
+                .setFromAlias(AliasProtos.Alias.newBuilder()
+                        .setValue("payer-alias@token.io") // user alias
+                        .setType(AliasProtos.Alias.Type.EMAIL)
+                        .build())
+                .setBankId("iron") // bank ID
+                .setCsrfToken(generateNonce()) // nonce for CSRF check
+                .build();
+
+        String requestId = payee.storeTokenRequestBlocking(request);
+
+        return requestId;
     }
 
     /**
@@ -49,9 +86,9 @@ public final class StoreAndRetrieveTokenRequestSample {
                 .setToMemberId(grantee.memberId())
                 .setRedirectUrl("https://token.io/callback") // callback URL
                 .setFromAlias(Alias.newBuilder()
-                                .setValue("grantor-alias@token.io") // user alias
-                                .setType(Alias.Type.EMAIL)
-                                .build())
+                        .setValue("grantor-alias@token.io") // user alias
+                        .setType(Alias.Type.EMAIL)
+                        .build())
                 .setBankId("iron") // bank ID
                 .setCsrfToken(generateNonce()) // nonce for CSRF check
                 .build();
@@ -70,5 +107,20 @@ public final class StoreAndRetrieveTokenRequestSample {
             TokenClient tokenClient,
             String requestId) {
         return tokenClient.retrieveTokenRequestBlocking(requestId);
+    }
+
+    /**
+     * Sets transfer destinations for a given token request.
+     *
+     * @param payee Payee Token member (the member requesting the transfer token be created)
+     * @param requestId token request id
+     * @param transferDestinations list of transfer destination
+     */
+    public static void setTokenRequestTransferDestinations(
+            Member payee,
+            String requestId,
+            List<TransferDestination> transferDestinations) {
+
+        payee.setTokenRequestTransferDestinationsBlocking(requestId, transferDestinations);
     }
 }
--- tpp-sample/src/test/java/io/token/sample/StoreAndRetrieveTokenRequestSampleTest.java
+++ tpp-sample/src/test/java/io/token/sample/StoreAndRetrieveTokenRequestSampleTest.java
@@ -1,14 +1,22 @@
 package io.token.sample;
 
+import static io.token.sample.StoreAndRetrieveTokenRequestSample.setTokenRequestTransferDestinations;
 import static io.token.sample.StoreAndRetrieveTokenRequestSample.storeAccessTokenRequest;
 import static io.token.sample.StoreAndRetrieveTokenRequestSample.storeTransferTokenRequest;
 import static io.token.sample.TestUtil.createClient;
 import static io.token.sample.TestUtil.randomAlias;
+import static io.token.util.Util.generateNonce;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import io.token.proto.common.transferinstructions.TransferInstructionsProtos.TransferDestination;
+import io.token.proto.common.transferinstructions.TransferInstructionsProtos.TransferDestination.DestinationCase;
 import io.token.tokenrequest.TokenRequest;
 import io.token.tpp.Member;
 import io.token.tpp.TokenClient;
+import io.token.tpp.tokenrequest.TokenRequestTransferDestinationsCallbackParameters;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -16,6 +24,12 @@ import org.junit.Test;
  * Sample to show how to store and retrieve token requests.
  */
 public class StoreAndRetrieveTokenRequestSampleTest {
+    private static String setTransferDestinationsUrl = "https://tpp-sample.com/callback/"
+            + "transferDestinations";
+    private static String setTransferDestinationsCallback = "https://tpp-sample.com/callback/"
+            + "transferDestinations?supportedTransferDestinationType=FASTER_PAYMENTS&"
+            + "supportedTransferDestinationType=SEPA&bankName=Iron&country=UK";
+
     @Test
     public void storeAndRetrieveTransferTokenTest() {
         try (TokenClient tokenClient = createClient()) {
@@ -33,6 +47,54 @@ public class StoreAndRetrieveTokenRequestSampleTest {
             String requestId = storeAccessTokenRequest(grantee);
             TokenRequest request = tokenClient.retrieveTokenRequestBlocking(requestId);
             assertThat(request).isNotNull();
+        }
+    }
+
+    @Test
+    public void storeTokenRequestAndSetTransferDestinationsTest() {
+        try (TokenClient tokenClient = createClient()) {
+            Member payee = tokenClient.createMemberBlocking(randomAlias());
+            String requestId = storeTransferTokenRequest(payee, setTransferDestinationsUrl);
+
+            TokenRequestTransferDestinationsCallbackParameters params =
+                    tokenClient.parseSetTransferDestinationsUrl(setTransferDestinationsCallback);
+
+            List<TransferDestination> transferDestinations = new ArrayList<>();
+            if (params.getSupportedTransferDestinationTypes()
+                    .contains(DestinationCase.FASTER_PAYMENTS)) {
+                TransferDestination destination = TransferDestination
+                        .newBuilder()
+                        .setFasterPayments(TransferDestination.FasterPayments
+                                .newBuilder()
+                                .setSortCode(generateNonce())
+                                .setAccountNumber(generateNonce())
+                                .build())
+                        .build();
+                transferDestinations.add(destination);
+            } else {
+                transferDestinations.add(TransferDestination
+                        .newBuilder()
+                        .setSepa(TransferDestination.Sepa
+                                .newBuilder()
+                                .setBic(generateNonce())
+                                .setIban(generateNonce())
+                                .build())
+                        .build());
+            }
+
+            setTokenRequestTransferDestinations(payee, requestId, transferDestinations);
+            TokenRequest request = tokenClient.retrieveTokenRequestBlocking(requestId);
+            assertThat(request).isNotNull();
+            assertThat(request
+                    .getTokenRequestPayload()
+                    .getTransferBody()
+                    .getInstructions()
+                    .getTransferDestinationsCount()).isNotEqualTo(0);
+            assertThat(request
+                    .getTokenRequestPayload()
+                    .getTransferBody()
+                    .getInstructions()
+                    .getTransferDestinationsList().get(0).hasFasterPayments()).isTrue();
         }
     }
 }
